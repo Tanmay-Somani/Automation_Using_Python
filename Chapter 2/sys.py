2c5c7a0583bf4342713b89c17347cb647d25981c
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-07 11:40:12 +0530

    im confused as to why the program isn't working

---
+++ Chapter 2/sys.py
@@ -0,0 +1,7 @@
+import sys
+while True:
+    response=input("if you want to exit the program")
+    if response=='exit':
+        print("the code is now being finished")
+        sys.exit()
+    print("you typed"+response+".")
