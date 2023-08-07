504d47a189a31271a403f129d6c741d9ce2449eb
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-07 12:23:02 +0530

    aborts

--- Chapter 3/guess_no.py
+++ Chapter 3/guess_no.py
@@ -1,3 +1,16 @@
 import random 
-print("I am thinking of a number between 1 to 20")
+print("I am thinking of a number between 1 and 20.")
 instance=random.randint(1,20)
+print(instance)
+cnt=0
+while True:
+    print("Take a guess")
+    choice=int(input())
+    if instance >choice:
+        print("your guess is too low")
+    elif instance < choice:
+        print("your guess is too high")
+    elif instance==choice:
+        print("Goodjob! you guessed my number in "+str(cnt)+" guesses.")
+        break
+    cnt+=1
