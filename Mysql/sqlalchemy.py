6f63dba743b9db2699a449746be6346577a4efba
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-11 10:09:01 +0530

    connector

--- Mysql/sqlalchemy.py
+++ Mysql/sqlalchemy.py
@@ -2,7 +2,10 @@ import mysql.connector
 from mysql.connector import Error
 from pprint import pprint
 import pandas as pd
+from sqlalchemy import create_engine
+
 try:
+    engine=create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format('root','davv','root','root'))
     connect=mysql.connector.connect(host='localhost',database='davv',user='root',password='root')
     cursor=connect.cursor()
     query="""select * from chapri;"""
