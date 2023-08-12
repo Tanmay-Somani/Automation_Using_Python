63070f697fbdc5dccf22edaac8a873422564b3c3
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 21:15:16 +0530

    added the graphs side by side

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -2,11 +2,9 @@ import networkx as nix
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_graph(graph):
-    pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10)
-    plt.show()
-
+def visualize_graph(graph,ax):
+    pos = nix.spring_layout(graph)
+    nix.draw(graph, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10,ax=ax)
 
 adj_matrix1 = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
 adj_matrix2 = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
@@ -14,8 +12,13 @@ adj_matrix2 = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 graph1 = nix.Graph(adj_matrix1)
 graph2 = nix.Graph(adj_matrix2)
 
-print("Graph 1:")
-visualize_graph(graph1)
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-print("Graph 2:")
-visualize_graph(graph2)
+axes[0].set_title("Graph 1")
+visualize_graph(graph1, axes[0])
+
+axes[1].set_title("Graph 2")
+visualize_graph(graph2, axes[1])
+
+plt.tight_layout()
+plt.show()
