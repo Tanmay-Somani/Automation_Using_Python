9345651e4698bb8fada6246cc0814da2baa11411
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 21:44:20 +0530

    checking the counts of matrices

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -6,22 +6,27 @@ def displ_grph(graph, ax):
     pos = nix.spring_layout(graph)
     nix.draw(graph, pos, with_labels=True, node_size=500, node_color='#5CA904', font_size=9, ax=ax)
 
+def vertex_count(adj_matrix):
+    return len(adj_matrix)
+
+def edge_count(adj_matrix):
+    return int(np.sum(adj_matrix) / 2)
+
 adj_matrix1 = np.array([
     [0, 1, 1, 0, 0],[1, 0, 1, 1, 0],[1, 1, 0, 0, 1],[0, 1, 0, 0, 1],[0, 0, 1, 1, 0]
 ])
 
 adj_matrix2 = np.array([
-    [0, 1, 0, 0, 1],[1, 0, 1, 1, 0],[0, 1, 0, 1, 0],[0, 1, 1, 0, 1],[1, 0, 0, 1, 0]
+    [0, 1, 0, 0, 1],[1, 0, 1, 1, 0],[0, 0, 0, 1, 0],[0, 1, 1, 0, 1],[1, 0, 0, 1, 0]
 ])
-
+if vertex_count(adj_matrix1)==vertex_count(adj_matrix2):
+    if edge_count(adj_matrix1)==edge_count(adj_matrix2):
+        print("hello")
 graph1 = nix.Graph(adj_matrix1)
 graph2 = nix.Graph(adj_matrix2)
-
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-
 axes[0].set_title("Graph 1")
 displ_grph(graph1, axes[0])
-
 axes[1].set_title("Graph 2")
 displ_grph(graph2, axes[1])
 
