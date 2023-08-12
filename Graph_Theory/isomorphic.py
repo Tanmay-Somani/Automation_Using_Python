d8226681e90b40c0151277c487e696aa4d08e5bf
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 21:08:36 +0530

    added the network

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -4,3 +4,7 @@ import numpy as np
 
 adj_matrix1 = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
 adj_matrix2 = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+
+graph1 = nix.Graph(adj_matrix1)
+graph2 = nix.Graph(adj_matrix2)
+
