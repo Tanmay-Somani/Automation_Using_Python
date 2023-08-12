3ba151f0d6fd53dfc391aeabcafa2447a0d140e8
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 22:02:43 +0530

    unoptimised but working code

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -2,9 +2,10 @@ import networkx as nix
 import matplotlib.pyplot as plt
 import numpy as np
 
-def displ_grph(graph, ax):
+def displ_grph(graph, ax,title):
     pos = nix.spring_layout(graph)
     nix.draw(graph, pos, with_labels=True, node_size=500, node_color='#5CA904', font_size=9, ax=ax)
+    ax.set_title(title)
 
 def vertex_count(adj_matrix):
     return len(adj_matrix)
@@ -34,28 +35,27 @@ adj_matrix2 = np.array([
     [0, 1, 0, 0, 1],[1, 0, 1, 1, 0],[0, 1, 0, 1, 0],[0, 1, 1, 0, 1],[1, 0, 0, 1, 0]
 ])
 if vertex_count(adj_matrix1)==vertex_count(adj_matrix2):
-    print("The vertices in both the matrix match")
+    V_M="The vertices in both the matrix match"
     if edge_count(adj_matrix1)==edge_count(adj_matrix2):
-        print("The edges in both the matrix match")
+        E_M="The edges in both the matrix match"
         if edge_connection_match(adj_matrix1, adj_matrix2):
             if degree_match(adj_matrix1, adj_matrix2):
-                print("The graphs are Isomorphic")
+                D_M="The graphs are Isomorphic"
             else:
-                print("The graphs are not Isomorphic")
+                D_M="The graphs are not Isomorphic"
         else:
             print("The graphs are not Isomorphic")
     else:
-        print("The graphs are not Isomorphic")
+        E_M="The graphs are not Isomorphic"
 else:
-    print("The graphs are not Isomorphic")                
+    V_M="The graphs are not Isomorphic"                
             
 graph1 = nix.Graph(adj_matrix1)
 graph2 = nix.Graph(adj_matrix2)
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-axes[0].set_title("Graph 1")
-displ_grph(graph1, axes[0])
-axes[1].set_title("Graph 2")
-displ_grph(graph2, axes[1])
+
+displ_grph(graph1, axes[0], "Graph 1\n" + V_M + "\n" + E_M + "\n" + D_M)
+displ_grph(graph2, axes[1], "Graph 2\n" + V_M + "\n" + E_M + "\n" + D_M)
 
 plt.tight_layout()
 plt.show()
