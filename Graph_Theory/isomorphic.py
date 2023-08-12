218b3206f67a93b00fdbd1f2c2ddf683d2223644
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 21:22:02 +0530

    added more color

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -2,9 +2,9 @@ import networkx as nix
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_graph(graph,ax):
+def displ_grph(graph,ax):
     pos = nix.spring_layout(graph)
-    nix.draw(graph, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10,ax=ax)
+    nix.draw(graph, pos, with_labels=True, node_size=500, node_color='#5CA904', font_size=10,ax=ax)
 
 adj_matrix1 = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
 adj_matrix2 = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
@@ -15,10 +15,10 @@ graph2 = nix.Graph(adj_matrix2)
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
 axes[0].set_title("Graph 1")
-visualize_graph(graph1, axes[0])
+displ_grph(graph1, axes[0])
 
 axes[1].set_title("Graph 2")
-visualize_graph(graph2, axes[1])
+displ_grph(graph2, axes[1])
 
 plt.tight_layout()
 plt.show()
