079917b5b37bfa7dedf5f240f510ae020005be25
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 21:52:16 +0530

    adding the comments

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -12,16 +12,31 @@ def vertex_count(adj_matrix):
 def edge_count(adj_matrix):
     return int(np.sum(adj_matrix) / 2)
 
+def edge_connection_match(adj_matrix1, adj_matrix2):
+    sorted_sums1 = sorted(np.sum(adj_matrix1, axis=0))
+    sorted_sums2 = sorted(np.sum(adj_matrix2, axis=0))
+    return sorted_sums1 == sorted_sums2
+
 adj_matrix1 = np.array([
     [0, 1, 1, 0, 0],[1, 0, 1, 1, 0],[1, 1, 0, 0, 1],[0, 1, 0, 0, 1],[0, 0, 1, 1, 0]
 ])
 
 adj_matrix2 = np.array([
-    [0, 1, 0, 0, 1],[1, 0, 1, 1, 0],[0, 0, 0, 1, 0],[0, 1, 1, 0, 1],[1, 0, 0, 1, 0]
+    [0, 1, 0, 0, 1],[1, 0, 1, 1, 0],[0, 1, 0, 1, 0],[0, 1, 1, 0, 1],[1, 0, 0, 1, 0]
 ])
 if vertex_count(adj_matrix1)==vertex_count(adj_matrix2):
+    print("The vertices in both the matrix match")
     if edge_count(adj_matrix1)==edge_count(adj_matrix2):
-        print("hello")
+        print("The edges in both the matrix match")
+        if edge_connection_match(adj_matrix1, adj_matrix2):
+            print("The graphs are Isomorphic")
+        else:
+            print("The graphs are not Isomorphic")
+    else:
+        print("The graphs are not Isomorphic")
+else:
+    print("The graphs are not Isomorphic")                
+            
 graph1 = nix.Graph(adj_matrix1)
 graph2 = nix.Graph(adj_matrix2)
 fig, axes = plt.subplots(1, 2, figsize=(10, 5))
