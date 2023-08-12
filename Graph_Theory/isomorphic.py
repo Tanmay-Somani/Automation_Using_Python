3bdea4fc9a7bbdeda610675c9bb8cc899335f2c0
Tanmay-Somani <tanmaysomani2003@gmail.com>
2023-08-12 21:07:18 +0530

    added the arrays

--- Graph_Theory/isomorphic.py
+++ Graph_Theory/isomorphic.py
@@ -2,3 +2,5 @@ import networkx as nix
 import matplotlib.pyplot as plt
 import numpy as np
 
+adj_matrix1 = np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]])
+adj_matrix2 = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
