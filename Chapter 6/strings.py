af6c0ab1730f6faa6ff51b9ef6adb6e036bae3fc
Tanmay Somani <tanmaysomani2003@gmail.com>
2023-08-22 09:39:28 +0530

    Update strings.py

--- Chapter 6/strings.py
+++ Chapter 6/strings.py
@@ -3,4 +3,5 @@ pprint('''Dear Alice,
 Eve's cat has been arrested for catnapping, cat burglary, and extortion.
 Sincerely,
 Bob''')          
-pprint("this is a line of code")     
+pprint("this is a line of code")  
+pprint("added a contri")
